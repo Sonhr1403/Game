b8f75d5da7fe3d68d429c37cbdcdcdbfc9d49223
Son1403 <sonhr1403@gmail.com>
2024-06-03 17:01:34 +0700

    meow

---
+++ assets/MeowHiLo/Scripts/MeowHiLo.Rules.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Rules extends cc.Component {
+
+    @property(cc.Node)
+    private listContents: cc.Node[] = [];
+
+    /////////////////////////////////////////
+
+    private currentRule: number = 0;
+
+    ///////////////////////////////////////////
+
+    private onClickRule(event, id: string){
+        curr
+    }
+}
